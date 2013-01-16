640ffeb573c744b0c949f286c218461a3dfed697
Mark A. Matienzo <mark@matienzo.org>
2013-01-16 13:42:35 -0500

    ftkimager: change homepage

--- ftkimager.rb
+++ ftkimager.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Ftkimager < Formula
   # binary only
-  homepage 'http://accessdata.com/support/adownloads'
+  homepage 'http://www.accessdata.com/support/product-downloads'
   url 'https://ad-zip.s3.amazonaws.com/ftkimager%203.1.1_Mac.u.zip'
   version '3.1.1'
   sha1 '3e4c6911dec66d050a14939a3e0469434c9d56ee'
