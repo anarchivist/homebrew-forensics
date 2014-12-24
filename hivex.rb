1e7ca25a240f4cdb77c11f5fca04809b43792942
Mark A. Matienzo <mark@matienzo.org>
2014-12-23 21:32:41 -0500

    hivex 1.3.11; add python bindings

--- hivex.rb
+++ hivex.rb
@@ -2,21 +2,30 @@ require 'formula'
 
 class Hivex < Formula
   homepage 'http://libguestfs.org/hivex.3.html'
-  url 'http://libguestfs.org/download/hivex/hivex-1.3.6.tar.gz'
-  sha1 'd3f89b854a36ac75b7db62096ad279f869294c0f'
+  url 'http://libguestfs.org/download/hivex/hivex-1.3.11.tar.gz'
+  sha1 '27861b1b72835647d57cf32fb6830f7d7282bae7'
   
-  depends_on 'autoconf'
-  depends_on 'automake'
-  depends_on 'libtool'
+  depends_on 'autoconf' => :build
+  depends_on 'automake' => :build
+  depends_on 'libtool' => :build
   depends_on 'ocaml'
-  depends_on 'pkgconfig'
+  depends_on 'pkgconfig' => :build
+  depends_on :python
+
+  option "with-python", "Install Python bindings"
 
   def install
     if MacOS.prefer_64_bit?
       ENV['ARCHFLAGS'] = "-arch i386 -arch x86_64"
     end
     # disable ruby bindings because of dumb build rules - FIXME
-    system "./configure", "--prefix=#{prefix}", "--disable-ruby"
+    args = [ "--prefix=#{prefix}", "--disable-ruby" ]
+
+    if build.with? "python"
+      args << "--enable-python"
+    end
+
+    system "./configure", *args
     # must run `make` before `make install` because of weird perl makefile 
     system "make"
     system "make install"
