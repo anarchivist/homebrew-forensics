68471434e5ab78d4c032fe9374a79174f6afb369
Mark A. Matienzo <mark@matienzo.org>
2015-09-17 00:33:49 -0400

    hivex: fixup build
    * explicitly declare additional requirements
    * add more specific fixme issues:
        * failed HEAD builds
        * failure w/ ruby bindings

--- hivex.rb
+++ hivex.rb
@@ -3,26 +3,35 @@ require 'formula'
 class Hivex < Formula
   homepage 'http://libguestfs.org/hivex.3.html'
   url 'http://libguestfs.org/download/hivex/hivex-1.3.11.tar.gz'
+  # FIXME: HEAD builds are currently broken :/
+  head 'https://github.com/libguestfs/hivex.git'
   sha1 '27861b1b72835647d57cf32fb6830f7d7282bae7'
   
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
+  depends_on 'gettext' => :build
   depends_on 'libtool' => :build
   depends_on 'ocaml'
   depends_on 'pkgconfig' => :build
   depends_on :python
+  depends_on 'readline' => :recommended
 
   option "with-python", "Install Python bindings"
+  option "with-ruby", "Install Ruby bindings"
 
   def install
+    ENV.append "LDFLAGS", "-L#{Formula["readline"].opt_lib} -lreadline" if build.with? "readline"
     if MacOS.prefer_64_bit?
       ENV['ARCHFLAGS'] = "-arch i386 -arch x86_64"
     end
-    # disable ruby bindings because of dumb build rules - FIXME
-    args = [ "--prefix=#{prefix}", "--disable-ruby" ]
 
-    if build.with? "python"
-      args << "--enable-python"
+    args = [ "--prefix=#{prefix}" ]
+    args << "--with-readline" if build.with? "readline"
+    args << "--enable-python" if build.with? "python"
+    args << "--disable-ruby" if build.without? "ruby"
+
+    if build.head?
+      system "./bootstrap"
     end
 
     system "./configure", *args
